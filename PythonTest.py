099a591d16ae73891a5ff533de324fa605401025
AssTrahanec <max197402@gmail.com>
2025-06-10 13:04:31 +0300

    python test

---
+++ PythonTest.py
@@ -0,0 +1,12 @@
+# TODO: Implement authentication | priority: high | category: security | assignee: @john | tags: auth,user
+def login():
+    pass
+
+"""
+TODO: Fix performance issue | priority: medium | category: optimization
+"""
+def process_data():
+    pass
+
+# Простой TODO без дополнительных параметров
+# TODO: Add logging
