9ba5f42a309307a7745dd2d6d1f230248ac2ecc9
AssTrahanec <91018687+AssTrahanec@users.noreply.github.com>
2025-06-10 13:14:20 +0300

    Update CSharpTest.cs

--- CSharpTest.cs
+++ CSharpTest.cs
@@ -7,13 +7,9 @@ namespace ExampleProject
         // todo: Добавить валидацию email | priority: high | category: feature
         public void RegisterUser(string email, string password)
         {
-            /* todo: Реализовать хеширование пароля | priority: high | category: security | assignee: @security-team */
             throw new NotImplementedException();
         }
 
-        /*
-         * todo: Добавить интеграцию с внешним API | priority: medium | category: feature | tags: api,integration
-         */
         public async Task<bool> ValidateUserCredentials(string email, string password)
         {
             // todo: Добавить логирование | priority: low
@@ -23,7 +19,7 @@ namespace ExampleProject
         /* todo: Реализовать восстановление пароля | priority: medium | category: feature | assignee: @john */
         private void SendPasswordResetEmail()
         {
-            // Здесь будет код отправки email
+            
         }
     }
 }
